0fbde947b7cd4a4c70bd24831fa03dc6ef9b24e9
Asphaltian <amirulhh2009@gmail.com>
2023-10-29 00:27:29 +0800

    Update LinkedPortalDoor.cs
    Made decompiled code slightly less bearable to read because of ILSpy

--- code/Entities/LinkedPortalDoor.cs
+++ code/Entities/LinkedPortalDoor.cs
@@ -1,7 +1,6 @@
 using System;
 using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using Editor;
 using Sandbox;
 using Sandbox.Internal;
@@ -118,15 +117,13 @@ public partial class LinkedPortalDoor : RenderEntity
     }
 
     public override void Spawn()
-    {
-        RuntimeHelpers.EnsureSufficientExecutionStack();
+    {        
         base.Transmit = TransmitType.Always;
     }
 
     [GameEvent.Tick]
     public void OnTick()
-    {
-        RuntimeHelpers.EnsureSufficientExecutionStack();
+    {       
         Partner = PartnerName.GetTarget<LinkedPortalDoor>();
 
         if (!Game.IsServer || (float)TimeSinceTeleported < 0.1f)
@@ -135,26 +132,26 @@ public partial class LinkedPortalDoor : RenderEntity
         }
 
         foreach (Entity entity in Entity.FindInBox(WorldBBox))
-        {
-            RuntimeHelpers.EnsureSufficientExecutionStack();
+        {           
             RenderBounds = new BBox(LocalBBox.Mins * Rotation, LocalBBox.Maxs * Rotation);
-            RuntimeHelpers.EnsureSufficientExecutionStack();
+            
             Vector3 eyePos = entity.Position + Vector3.Up * 64f;
+
             if (WorldBBox.Contains(eyePos))
             {
                 if (entity.Velocity.Length < 50f)
                 {
                     break;
                 }
+
                 Transform entityLocal = base.Transform.ToLocal(entity.Transform);
-                RuntimeHelpers.EnsureSufficientExecutionStack();
-                entity.Position = Partner.Transform.PointToWorld(entityLocal.Position);
-                RuntimeHelpers.EnsureSufficientExecutionStack();
-                entity.Position += Vector3.Up;
-                RuntimeHelpers.EnsureSufficientExecutionStack();
+                
+                entity.Position = Partner.Transform.PointToWorld(entityLocal.Position);                
+                entity.Position += Vector3.Up;               
                 entity.AngularVelocity = new Angles(0f, 90f, 0f);
+
                 Rotation.LookAt((Partner.Position - Position).Normal, Vector3.Up);
-                RuntimeHelpers.EnsureSufficientExecutionStack();
+                
                 TimeSinceTeleported = 0f;
             }
         }
@@ -165,27 +162,20 @@ public partial class LinkedPortalDoor : RenderEntity
         float halfHeight = Camera.ZNear * MathF.Tan((Camera.FieldOfView * 0.5f).DegreeToRadian());
         float dstToNearClipPlaneCorner = new Vector3(halfHeight * Screen.Aspect, halfHeight, Camera.ZNear).Length * 0.0254f;
         float dot = Vector3.Dot(Rotation.Forward, (Position - Camera.Position).Normal);
-        RuntimeHelpers.EnsureSufficientExecutionStack();
-        scale = new Vector3(1f, 1f, 1f) * Rotation;
-        RuntimeHelpers.EnsureSufficientExecutionStack();
+        
+        scale = new Vector3(1f, 1f, 1f) * Rotation;        
         offset = Vector3.Forward * (dstToNearClipPlaneCorner * ((dot > 0f) ? 1f : (-1f)) * 20f);
     }
 
     public override void UpdateSceneObject(SceneObject obj)
-    {
-        RuntimeHelpers.EnsureSufficientExecutionStack();
+    {       
         base.UpdateSceneObject(obj);
-        RuntimeHelpers.EnsureSufficientExecutionStack();
-        obj.Flags.IsTranslucent = false;
-        RuntimeHelpers.EnsureSufficientExecutionStack();
-        obj.Flags.IsOpaque = true;
-        RuntimeHelpers.EnsureSufficientExecutionStack();
+        
+        obj.Flags.IsTranslucent = false;        
+        obj.Flags.IsOpaque = true;        
         obj.Flags.BloomLayer = false;
-        RuntimeHelpers.EnsureSufficientExecutionStack();
-        obj.Flags.CastShadows = false;
-        RuntimeHelpers.EnsureSufficientExecutionStack();
-        obj.Flags.OverlayLayer = false;
-        RuntimeHelpers.EnsureSufficientExecutionStack();
+        obj.Flags.CastShadows = false;       
+        obj.Flags.OverlayLayer = false;      
         obj.RenderLayer = SceneRenderLayer.Default;
     }
 
@@ -193,16 +183,14 @@ public partial class LinkedPortalDoor : RenderEntity
     public static void RenderAllPortals()
     {
         foreach (LinkedPortalDoor portal in Entity.All.OfType<LinkedPortalDoor>())
-        {
-            RuntimeHelpers.EnsureSufficientExecutionStack();
+        {           
             StartActive = true;
-            RuntimeHelpers.EnsureSufficientExecutionStack();
-            portal.colorTarget = Texture.CreateRenderTarget("LinkedPortalDoorTexture:Color", ImageFormat.RGB565, Screen.Size * RenderScale, portal.colorTarget);
-            RuntimeHelpers.EnsureSufficientExecutionStack();
-            portal.depthTarget = Texture.CreateRenderTarget("LinkedPortalDoorTexture:Depth", ImageFormat.D24S8, Screen.Size * RenderScale, portal.depthTarget);
-            RuntimeHelpers.EnsureSufficientExecutionStack();
+            
+            portal.colorTarget = Texture.CreateRenderTarget("LinkedPortalDoorTexture:Color", ImageFormat.RGB565, Screen.Size * RenderScale, portal.colorTarget);           
+            portal.depthTarget = Texture.CreateRenderTarget("LinkedPortalDoorTexture:Depth", ImageFormat.D24S8, Screen.Size * RenderScale, portal.depthTarget);    
+            
             portal.RenderScene(RenderTarget.From(portal.colorTarget, portal.depthTarget));
-            RuntimeHelpers.EnsureSufficientExecutionStack();
+            
             StartActive = false;
         }
     }
@@ -215,42 +203,37 @@ public partial class LinkedPortalDoor : RenderEntity
 			Transform cameraTransform = new Transform(Camera.Position, Camera.Rotation);
 			Transform localCamera = base.Transform.ToLocal(cameraTransform);
 			Transform endCamera = Partner.Transform.ToWorld(localCamera);
+
 			renderCamera = new SceneCamera("LinkedPortalDoorCamera");
-			RuntimeHelpers.EnsureSufficientExecutionStack();
 			renderCamera.World = Game.SceneWorld;
-			RuntimeHelpers.EnsureSufficientExecutionStack();
 			renderCamera.Position = endCamera.Position;
-			RuntimeHelpers.EnsureSufficientExecutionStack();
 			renderCamera.Rotation = endCamera.Rotation;
-			RuntimeHelpers.EnsureSufficientExecutionStack();
 			renderCamera.FieldOfView = Camera.FieldOfView;
-			RuntimeHelpers.EnsureSufficientExecutionStack();
 			renderCamera.ZNear = 1f;
-			RuntimeHelpers.EnsureSufficientExecutionStack();
 			renderCamera.ZFar = 2048f;
-			RuntimeHelpers.EnsureSufficientExecutionStack();
 			renderCamera.BackgroundColor = Color.Black;
-			RuntimeHelpers.EnsureSufficientExecutionStack();
 			renderCamera.EnablePostProcessing = true;
+
 			float distance = (Position - Camera.Position).Length;
 			Plane clipPlane2 = new Plane(endCamera.Rotation.Forward, (double)distance - 256.0);
-			RuntimeHelpers.EnsureSufficientExecutionStack();
+			
 			SetClipPlane(clipPlane2);
-			RuntimeHelpers.EnsureSufficientExecutionStack();
+			
 			Graphics.RenderToTexture(renderCamera, renderTarget.ColorTarget);
 		}
 		void SetClipPlane(Plane clipPlane)
 		{
 			RenderAttributes attributes = renderCamera.Attributes;
+
 			string i = "EnableClipPlane";
 			bool value = true;
-			RuntimeHelpers.EnsureSufficientExecutionStack();
+			
 			attributes.Set(in i, in value);
 			RenderAttributes attributes2 = renderCamera.Attributes;
-			RuntimeHelpers.EnsureSufficientExecutionStack();
+			
 			i = "ClipPlane0";
 			Vector4 value2 = new Vector4(in clipPlane.Normal, clipPlane.Distance);
-			RuntimeHelpers.EnsureSufficientExecutionStack();
+			
 			attributes2.Set(in i, in value2);
 		}
 	}
@@ -260,21 +243,22 @@ public partial class LinkedPortalDoor : RenderEntity
 		if (!StartActive)
 		{
 			_ = (Position - Partner.Position).Normal;
-			RuntimeHelpers.EnsureSufficientExecutionStack();
+			
 			RenderAttributes attributes = Graphics.Attributes;
+
 			string k = "ColorTarget";
 			int mip = -1;
+
 			attributes.Set(in k, in colorTarget, in mip);
-			RuntimeHelpers.EnsureSufficientExecutionStack();
+			
 			Graphics.GrabDepthTexture("DepthTarget");
-			RuntimeHelpers.EnsureSufficientExecutionStack();
+			
 			ProtectFromClipping(out var scale, out var offset);
+
 			VertexBuffer vertexBuffer = new VertexBuffer();
-			RuntimeHelpers.EnsureSufficientExecutionStack();
-			vertexBuffer.Init(useIndexBuffer: true);
-			RuntimeHelpers.EnsureSufficientExecutionStack();
-			vertexBuffer.AddCube(Vector3.Zero + offset, LocalBBox.Size * scale, Rotation.Identity);
-			RuntimeHelpers.EnsureSufficientExecutionStack();
+			
+			vertexBuffer.Init(useIndexBuffer: true);			
+			vertexBuffer.AddCube(Vector3.Zero + offset, LocalBBox.Size * scale, Rotation.Identity);			
 			vertexBuffer.Draw(material);
 		}
 	}
