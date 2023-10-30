a16c4c014eedd8434b3c5f187e338e64fd4c428e
Asphaltian <amirulhh2009@gmail.com>
2023-10-30 18:56:23 +0800

    Added Fonts

--- code/Game.cs
+++ code/Game.cs
@@ -93,50 +93,6 @@ partial class TheStanleyParable : GameManager
     [ConCmd.Server("tsp_map")]
     public static void MapCmd()
     {
-        SetRandomMugColors(); 
-
         
-    }
-
-    private static void SetRandomMugColors()
-    {
-        System.Random random = new System.Random();
-
-        string[] coffeecup1Colors = new string[]
-        {
-            "208 224 152 1",
-            "186 141 235 1",
-            "147 230 180 1",
-            "232 145 157 1"
-        };
-
-        string[] coffeecup2Colors = new string[]
-        {
-            "147 188 230 1",
-            "228 197 148 1",
-            "232 145 157 1",
-            "232 145 157 1"
-        };
-
-        string[] coffeecup3Colors = new string[]
-        {
-            "232 145 157 1",
-            "140 152 236 1",
-            "236 224 140 1"
-        };
-
-        // Randomly select colors for each mug
-        string randomColor1 = coffeecup1Colors[random.Next(coffeecup1Colors.Length)];
-        string randomColor2 = coffeecup2Colors[random.Next(coffeecup2Colors.Length)];
-        string randomColor3 = coffeecup3Colors[random.Next(coffeecup3Colors.Length)];
-
-        Entity coffeeCup1 = FindByName("coffeecup1");
-        Entity coffeeCup2 = FindByName("coffeecup2");
-        Entity coffeeCup3 = FindByName("coffeecup3");
-
-        // Set the selected colors for the mugs using FireInput
-        coffeeCup1.FireInput("SetColor", (Entity)ConsoleSystem.Caller.Client.Pawn, randomColor1);
-        coffeeCup2.FireInput("SetColor", (Entity)ConsoleSystem.Caller.Client.Pawn, randomColor2);
-        coffeeCup3.FireInput("SetColor", (Entity)ConsoleSystem.Caller.Client.Pawn, randomColor3);
     }
 }
