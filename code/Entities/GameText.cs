9e96e8a7b082c9ad65e69856bf5a4c4c068dfacf
Asphaltian <amirulhh2009@gmail.com>
2023-10-31 22:55:32 +0800

    Added More Placeholders

---
+++ code/Entities/GameText.cs
@@ -0,0 +1,12 @@
+﻿using Editor;
+using Sandbox;
+
+/// <summary>
+/// An entity that displays text on player's screens.
+/// </summary>
+[Library("game_text"), HammerEntity]
+[EditorSprite("editor/game_text.vmat")]
+[Title("Text"), Category("Game"), Icon("text_fields")]
+public partial class GameText : Entity
+{
+}
